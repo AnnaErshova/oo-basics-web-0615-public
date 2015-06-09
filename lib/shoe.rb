1bcb720ee7418f2a9105ceea5239f5e26196c7cb
AnnaErshova <anna.ershova@gmail.com>
2015-06-09 15:57:58 -0400

    all done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+# Open `spec/02_shoe_spec.rb` and start making the tests pass. Write your code in `shoe.rb`.
+# To run the specs, type `rspec spec/02_shoe_spec.rb` on your command line.
+
+class Shoe
+
+  BRANDS = []
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand) # put it in initialize because we seem to be initializing with a brand
+    @brand = brand
+    if BRANDS.include?(brand) 
+    else BRANDS << brand
+    end #could not get uniq to work here
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new" # needs an instance variabe since it sits within one class
+  end
+
+end
+
